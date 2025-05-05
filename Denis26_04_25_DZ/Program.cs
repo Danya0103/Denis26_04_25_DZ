6e94620718df65b7b51a2adf4314b3e2d097ae6b
Danya0103 <user@WIN-LRBB7CDB9IA>
2025-05-05 23:21:48 +0300

    Добавьте файлы проекта.

---
+++ Denis26_04_25_DZ/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+/*
+class Program {
+
+    static double MultiplyArrayElements(double[] arr) {
+
+        double result = 1;
+
+        for (int i = 0; i < arr.Length; i++) {
+
+            result *= arr[i];
+
+        }
+
+        return result;
+
+    }
+
+    static void Main(string[] args) {
+
+        double[] number = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10};
+
+        double numbersMultiplication = MultiplyArrayElements(number);
+
+        Console.WriteLine($"Добуток всіх чисел у масиві: {numbersMultiplication}");
+
+    }
+}
+*/
+
+/*
+class Program {
+    static void Main(string[] args) {
+
+        double[] temperatures = {15.5, 17.8, 20.2, 18.1, 19.4, 21.3, 16.7};
+
+        double maxTemperature = temperatures[0];
+
+        for (int i = 1; i < temperatures.Length; i++) {
+
+            if (temperatures[i] > maxTemperature) {
+
+                maxTemperature = temperatures[i];
+
+            }
+        }
+
+        double totalTemperature = 0;
+        for (int i = 0; i < temperatures.Length; i++) {
+
+            totalTemperature += temperatures[i];
+
+        }
+
+        double averageTemperature = totalTemperature / temperatures.Length;
+
+        Console.WriteLine($"Максимальна температура за тиждень: {maxTemperature}");
+        Console.WriteLine($"Середня температура за тиждень: {averageTemperature}");
+
+    }
+}
+*/
